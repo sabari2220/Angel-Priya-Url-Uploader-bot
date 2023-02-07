437cf7ab3a3fa0eb9f99d4f6bf4a6544499e3bdc
sabari2220 <123808233+sabari2220@users.noreply.github.com>
2023-02-07 18:54:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@
 import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger =(urlUploadBot)
+logger =(__UrlUploadBot__)
 
 import os
 
