147f4b3cb4237fb6aef397c3f96966f522a29ca0
sabari2220 <123808233+sabari2220@users.noreply.github.com>
2023-02-07 18:54:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) Thank you @LazyDeveloperr 
+# (c) Thank you @Sabari023
 
 # the logging things
 import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger =(url_Upload_Bot)
+logger =(urlUploadBot)
 
 import os
 
